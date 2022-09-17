d35f69c022a66446a78ccba10d52d99a93a9bd73
BnOiDev <bnoo.almeida@gmail.com>
2022-09-17 19:09:34 -0300

    ajustes

--- App.tsx
+++ App.tsx
@@ -1,43 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
+import { Background } from './src/Components/Background';
 
-interface ButtonProps{
-  title: string;
-}
-
-function Button (props: ButtonProps){
-  return(
-    <TouchableOpacity>
-      <Text>
-       {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello NLW</Text>
-
-        <Button title='Send 1'/>
-        <Button title='Send 2'/>
-        <Button title='Send 4'/>
-        <Button title='Hello Word'/>
-
-      <StatusBar style="auto" backgroundColor='#000' />
-    </View>
+    <Background>
+      
+    </Background>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-   
