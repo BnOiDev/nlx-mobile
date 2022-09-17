709918201b3e741ac09c1410e088b01e18ed71c6
BnOiDev <bnoo.almeida@gmail.com>
2022-09-17 19:38:06 -0300

    ponto de segurança

--- App.tsx
+++ App.tsx
@@ -1,10 +1,28 @@
+import { StatusBar } from 'react-native';
+import {useFonts,Inter_400Regular,Inter_600SemiBold,Inter_700Bold,Inter_900Black} from '@expo-google-fonts/inter';
 
+import { Home } from './src/screens/Home';
+import {Loading} from './src/Components/Loading';
 import { Background } from './src/Components/Background';
 
-
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    useFonts,
+      Inter_400Regular,
+      Inter_600SemiBold,
+      Inter_700Bold,
+      Inter_900Black
+  });
+
   return (
     <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {fontsLoaded ? <Home/>: <Loading/>}
       
     </Background>
   );
